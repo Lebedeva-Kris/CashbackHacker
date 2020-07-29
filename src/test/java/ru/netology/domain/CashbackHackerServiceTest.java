22ab279e8c070e745698473a921ad86dd0d9dc3f
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-29 20:56:35 +0300

    Junit4 tests added

--- src/test/java/ru/netology/domain/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/domain/CashbackHackerServiceTest.java
@@ -9,18 +9,51 @@ import static org.junit.Assert.assertEquals;
 public class CashbackHackerServiceTest {
     public CashbackHackerService service = new CashbackHackerService();
 
+    @Test
+    public void shouldRemainIfPaymentBoundary() {
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldRemainIfPaymentLessThanBoundary() {
+        int amount = 700;
+
+        int actual = service.remain(amount);
+        int expected = 300;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldRemainIfPaymentZero() {
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+
+    }
+
     @org.junit.jupiter.api.Test
-    public void shouldRemainIfAmountZero() {
+    public void shouldRemainIfAmountZeroJupiter() {
         org.junit.jupiter.api.Assertions.assertEquals(1000, service.remain(0));
     }
 
     @org.junit.jupiter.api.Test
-    public void shouldRemainIfAmountLessThanBoundary() {
+    public void shouldRemainIfAmountLessThanBoundaryJupiter() {
         org.junit.jupiter.api.Assertions.assertEquals(300, service.remain(700));
     }
 
     @org.junit.jupiter.api.Test
-    public void shouldRemainIfAmountBoundary() {
+    public void shouldRemainIfAmountBoundaryJupiter() {
         org.junit.jupiter.api.Assertions.assertEquals(0, service.remain(1000));
     }
 
